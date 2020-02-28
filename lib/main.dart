7666407850108d031bfd795b10008903c12dc902
EMMANOEL <emmanoel@asav.org.br>
2020-02-28 15:00:58 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+  Firestore.instance.collection("mensagens").document("msg1").setData({
+    'texto': 'Olá',
+
+  });
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
